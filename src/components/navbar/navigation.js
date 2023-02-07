9a9c01ce4e6f0eeb3db85880984c467ac73204e0
Dell Pc <mudasir9757@gmail.com>
2023-02-07 15:30:19 +0500

    Add styles to navigation

--- src/components/navbar/navigation.js
+++ src/components/navbar/navigation.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import './navigation.css';
+import { FiSettings } from 'react-icons/fi';
+import { FaMicrophone } from 'react-icons/fa';
+import { IoIosArrowBack } from 'react-icons/io';
 
 const Navigation = () => {
   const Navigation = useNavigate();
   return (
     <div className="navigation">
       <div onClick={() => Navigation('/')} aria-hidden="true">
-        <img className="icon-btn" src="" alt="Back Button" />
+        <IoIosArrowBack className="icon-btn" alt="Back Button" />
       </div>
+
       <h2 className="app-heading">Watch Movies</h2>
-      <div className="feature-icons">
-        <img className="icon-btn" src="" alt="Microphone Icon" />
-        <img className="icon-btn" src="" alt="Setting Icon" />
-      </div>
+      <div className="feature-icons" />
+      <FaMicrophone className="icon-btn" alt="Microphon Icon" />
+      <FiSettings className="icon-btn" alt="Setting Icon" />
     </div>
   );
 };
