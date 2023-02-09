42484014f607c717839e484d2830c145408f999b
Dell Pc <mudasir9757@gmail.com>
2023-02-09 11:06:37 +0500

    Add Test for Home Page Movies Title list

--- src/components/homePage/MoviesTitleItems.js
+++ src/components/homePage/MoviesTitleItems.js
@@ -6,7 +6,7 @@ import { RiMovieFill } from 'react-icons/ri';
 import { fetchDetailData } from '../../redux/fetch-movies-details';
 import './homepage.css';
 
-const MoviesTitleItems = (props) => {
+const MoviesTitleItems = (props) => {  
   const { id, title } = props;
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -16,7 +16,7 @@ const MoviesTitleItems = (props) => {
   };
 
   return (
-    <div className="container1" data-testid="homepageitem">
+    <div className="container1" data-testid="home-page">
       <div className="item">
         <RiMovieFill id={id} size={50} onClick={showDetails} className="movie-icon" alt="movies icon" />
         <h3 className="title-home">{title}</h3>
@@ -26,8 +26,8 @@ const MoviesTitleItems = (props) => {
 };
 
 MoviesTitleItems.propTypes = {
-  title: PropTypes.string.isRequired,
-  id: PropTypes.number.isRequired,
+  title: PropTypes.string,
+  id: PropTypes.number,
 };
 
 export default MoviesTitleItems;
